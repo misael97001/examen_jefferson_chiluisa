b3ee5e3d980a5a8a4e931e95114b45017a836e94
Misael97001 <chiluisajefferson97@gmail.com>
2026-05-10 08:13:14 -0500

    First commit

---
+++ src/main/java/com/krakedev/examen_jefferson_chiluisa/ExamenJeffersonChiluisaApplication.java
@@ -0,0 +1,13 @@
+package com.krakedev.examen_jefferson_chiluisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenJeffersonChiluisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenJeffersonChiluisaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krakedev/examen_jefferson_chiluisa/ExamenJeffersonChiluisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krakedev.examen_jefferson_chiluisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenJeffersonChiluisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
