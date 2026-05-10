58c984501cf1e3a1bf6c8f3b833deaf7ba30a758
Misael97001 <chiluisajefferson97@gmail.com>
2026-05-10 08:24:49 -0500

    clase vendedor

---
+++ src/main/java/com/krakedev/examen_jefferson_chiluisa/modelos/Vendedor.java
@@ -0,0 +1,57 @@
+package com.krakedev.examen_jefferson_chiluisa.modelos;
+
+public class Vendedor {
+
+	private String cedula;
+    private int numeroVentas;
+    private double sueldoFijo;
+    private double comisionPorVenta;
+    
+
+    public Vendedor(String cedula, String tipo) {
+        this.cedula = cedula;
+    }
+
+    public double calcularSueldo() {
+        return sueldoFijo;
+    }
+
+    @Override
+    public String toString() {
+        return "Vendedor [Cédula: " + cedula + ", Ventas: " + numeroVentas + "]";
+    }
+
+    
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public int getNumeroVentas() {
+        return numeroVentas;
+    }
+
+    public void setNumeroVentas(int numeroVentas) {
+        this.numeroVentas = numeroVentas;
+    }
+
+    public double getSueldoFijo() {
+        return sueldoFijo;
+    }
+
+    public void setSueldoFijo(double sueldoFijo) {
+        this.sueldoFijo = sueldoFijo;
+    }
+
+    public double getComisionPorVenta() {
+        return comisionPorVenta;
+    }
+
+    public void setComisionPorVenta(double comisionPorVenta) {
+        this.comisionPorVenta = comisionPorVenta;
+    }
+
+}
